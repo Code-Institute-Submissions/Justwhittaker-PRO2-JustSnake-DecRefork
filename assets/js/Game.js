f2180083f905ca5fbc4df6f5df9639879b4b86e3
Justwhittaker <just.whittaker@gmail.com>
2020-04-17 11:49:49 +0000

    js-movement

--- assets/js/Game.js
+++ assets/js/Game.js
@@ -109,27 +109,26 @@ if (snake.y < 0) {
 
 // give movement on keyboard
 document.addEventListener('keydown',function(e) {
-    //left arrow
-    if (e.which === 37 && snake.dx === 0) {
-        snake.dx = -grid;
-        snake.dy = 0;
-    }
-    //up arrow
-    else if (e.which === 38 && snake.dy === 0) {
-        snake.dx = -grid;
-        snake.dy = 0;
-    }
-    // right arrow
-    else if (e.which === 39 && snake.dx === 0){
-        snake.dx = grid;
-        snake.dy = 0;
-    }
-    //down arrow
-    else if (e.which === 40 && snake.dy === 0){
-        snake.dx = grid;
-        snake.dy = 0;
-    }
-
+// left arrow key
+  if (e.which === 37 && snake.dx === 0) {
+    snake.dx = -grid;
+    snake.dy = 0;
+  }
+  // up arrow key
+  else if (e.which === 38 && snake.dy === 0) {
+    snake.dy = -grid;
+    snake.dx = 0;
+  }
+  // right arrow key
+  else if (e.which === 39 && snake.dx === 0) {
+    snake.dx = grid;
+    snake.dy = 0;
+  }
+  // down arrow key
+  else if (e.which === 40 && snake.dy === 0) {
+    snake.dy = grid;
+    snake.dx = 0;
+  }
 });
 //start game
 requestAnimationFrame(loop);
