6b6301de04a90a684097472a994f2de56d2abd33
Justin Whittaker <just.whittaker@gmail.com>
2020-06-18 16:33:12 +0000

    add all directions and food

--- v2/game.js
+++ v2/game.js
@@ -6,13 +6,21 @@ var ctx;
 var direction = {
     NORTH: 0,
     EAST: 1,
-}
+    SOUTH: 2,
+    WEST: 3
+};
 
 var snake = {
     direction: direction.NORTH,
+    parts: [
+        { position: { x: 10, y: 10 } }
+    ]
+};
+
+var food = {
     position: {
-        x: 10,
-        y: 10
+        x: -1,
+        y: -1
     }
 };
 
@@ -20,6 +28,8 @@ var snake = {
 
 key('w', goNorth);
 key('d', goEast);
+key('s', goSouth);
+key('a', goWest);
 
 function goNorth() {
     setSnakeDirection(direction.NORTH);
@@ -29,6 +39,14 @@ function goEast() {
     setSnakeDirection(direction.EAST);
 }
 
+function goSouth() {
+    setSnakeDirection(direction.SOUTH);
+}
+
+function goWest() {
+    setSnakeDirection(direction.WEST);
+}
+
 function initWorld() {
     canvas = $('#game-panel')[0];
     ctx = canvas.getContext('2d');
@@ -36,19 +54,38 @@ function initWorld() {
 
 function clearWorld() {
     ctx.beginPath();
-    ctx.fillStyle = "#000000";
+    ctx.fillStyle = '#000000';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 }
 
+function checkFoodHit() {
+    if (snake.position.x == food.position.x && snake.position.y == food.position.y) {
+        addFood();
+        growSnake();
+    }
+}
+
+function addFood() {
+    food.position.x = Math.floor(Math.random() * ((canvas.width / 10) - 1));
+    food.position.y = Math.floor(Math.random() * ((canvas.height / 10) - 1));
+}
+
+function growSnake() {
+    snake.length = snake.length + 1;
+}
+
 function loop() {
-    moveSnake()
+    moveSnake();
+    checkFoodHit();
     clearWorld();
     drawSnake();
+    drawFood();
 }
 
 function start() {
     initWorld();
-    setInterval(loop, 500);
+    addFood();
+    setInterval(loop, 300);
 }
 
 // Snake
@@ -64,13 +101,25 @@ function moveSnake() {
     if (snake.direction == direction.EAST) {
         snake.position.x = snake.position.x + 1;
     }
+    if (snake.direction == direction.SOUTH) {
+        snake.position.y = snake.position.y + 1;
+    }
+    if (snake.direction == direction.WEST) {
+        snake.position.x = snake.position.x - 1;
+    }
 }
 
 function drawSnake() {
-    ctx.fillStyle = "#00FF00";
-    ctx.fillRect(snake.position.x * 10, snake.position.y * 10, 10, 10);
+    ctx.fillStyle = '#00FF00';
+    for (var i = 0; i < snake.length; i++) {
+        ctx.fillRect(snake.position.x * 10, snake.position.y * 10, 10, 10);
+    }
 }
 
+function drawFood() {
+    ctx.fillStyle = 'red';
+    ctx.fillRect(food.position.x * 10, food.position.y * 10, 10, 10);
+}
 // main
 
 $(document).ready(start);
